38d688a958185355308b8fe8b91ace9805421e49
Kevin Gill <kevingill@w134-87-177-092.wireless.uvic.ca>
2014-06-30 12:16:14 -0700

    Jquery code is now in main.js (client side)

--- client/main.js
+++ client/main.js
@@ -1,1 +1,26 @@
 // This is the main file for javascript functions used by the client
+$(document).ready(function(){
+      $('#datepicker').datepicker({
+        dateFormat: 'yy-mm-dd',        
+        inline: true,
+        onSelect: function(){
+          var day = $("#datepicker").datepicker('getDate').getDate();
+          var month = $("#datepicker").datepicker('getDate').getMonth() + 1;
+          var year = $("#datepicker").datepicker('getDate').getFullYear();
+          var fullDate = year + "-" + month + "-" + day;
+
+          console.log(fullDate);
+        }
+      });
+
+      $('#bookAppointment').click(function(){
+        var selectPhys = document.getElementById('select-physician');
+        var physician = selectPhys.options[selectPhys.selectedIndex].value;
+
+        var selectTime = document.getElementById('select-time');
+        var time = selectTime.options[selectTime.selectedIndex].value;
+
+        console.log(physician);
+        console.log(time);
+      });
+    });
