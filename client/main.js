1e6dc47c7cfa60e70a5cd85ade1429a2c309b3fe
Kevin Gill <kevingill@w134-87-177-092.wireless.uvic.ca>
2014-06-30 13:30:25 -0700

    confirm page is pretty much working except for reason for visit

--- client/main.js
+++ client/main.js
@@ -8,16 +8,18 @@ $(document).ready(function(){
           var month = $("#datepicker").datepicker('getDate').getMonth() + 1;
           var year = $("#datepicker").datepicker('getDate').getFullYear();
           var fullDate = year + "-" + month + "-" + day;
+          $('#myDate').html(fullDate);
         }
       });
 
       $('#bookAppointment').click(function(){
         var selectPhys = document.getElementById('select-physician');
-        var physician = selectPhys.options[selectPhys.selectedIndex].value;
+        var physician = selectPhys.options[selectPhys.selectedIndex];
 
         var selectTime = document.getElementById('select-time');
-        var time = selectTime.options[selectTime.selectedIndex].value;
+        var time = selectTime.options[selectTime.selectedIndex];
         
-        $('#myPhys').html(physician);
+        $('#myPhys').html(physician.text);
+        $('#myApptTime').html(time.text);
       });
 });
