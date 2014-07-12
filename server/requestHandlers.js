9eae73ac15a17c2a96bce28995d967c5c1d81746
spatchkaa <spatchkaa@gmail.com>
2014-07-12 13:23:39 -0700

    added get request for appointments

--- server/requestHandlers.js
+++ server/requestHandlers.js
@@ -1,5 +1,5 @@
 var DAO = require("./DAO.js");
-
+var url = require("url");
 
 function appointmentHandler(dataObj){
   // Here we handle post requests to appointments
@@ -17,6 +17,19 @@ function appointmentHandler(dataObj){
       DAO.create("appointment", dataObj);
     });
   }
+  if(dataObj.request.method == "GET"){
+    var urlparts = url.parse(dataObj.request.url, true);
+    var netlink_id = urlparts.query;
+    DAO.read("appointment", netlink_id, dataObj);
+  }
+}
+
+function slotsHandler(dataObj){
+  // Handle GET request
+  if(dataObj.request.method == "GET"){
+    // This means we want to search for all slots where booked = false
+    DAO.read("slot", {booked: false}, dataObj);
+  }
 }
 
 // If we want to remove code duplication, do this!
@@ -28,7 +41,7 @@ function appointmentHandler(dataObj){
 // so we can call the handlers in main
 function setHandle(handle){
   handle['/appointment'] = appointmentHandler;
-
+  handle['/slots'] = slotsHandler;
 }
 
 exports.setHandle = setHandle;
