7f0c6eb18e8700f98355c512b44a324ecb27b2ad
Chris <chriscook25@gmail.com>
2014-07-13 14:24:39 -0700

    login page stores credentials and uses them in viewAppts

--- client/main.js
+++ client/main.js
@@ -12,6 +12,9 @@ var time;
 var description;
 var key;
 
+var userLogin;
+var userPassword;
+
 $(document).ready(function(){
   $('#datepicker').datepicker({
     dateFormat: 'yy-mm-dd',        
@@ -26,23 +29,34 @@ $(document).ready(function(){
   });
 
   $('#viewAppoints').click(function(){
-    jsonStuff = $.getJSON("http://127.0.0.1:8888/appointment?netlink_id=JOEY", function(){
+    jsonStuff = $.getJSON(serverURL+"/appointment?netlink_id="+userLogin, function(){
       var temp = jsonStuff["responseText"];
       var temp2 = JSON.parse(temp);
 
       console.log(Object.keys(temp2).length);
       console.log(temp2[0]["netlink_id"]);
 
+      // temp2[]["date"].sort();
+
       var jsonLength = Object.keys(temp2).length;
 
       for (var i = 0; i < jsonLength; i++) {
         $("#appointmentList").append('<li id="appointment'+i+'"></li>');
         $("#appointment"+i).html("Appointment on "+temp2[i]["date"]+" with "+temp2[i]["physician"]);
+        $("#appointment"+i).append("<br>Reason: "+temp2[i]["description"]);
 
         console.log(i);
       } 
 
     });
+  });
+
+  $('#submitButton').click(function(){
+    userLogin = $('#username').val();
+    userPassword = $('#password').val();
+
+    console.log(userLogin);
+    console.log(userPassword);
   });
 
 //       $('#loginForm:#submitButton').click(function(){
